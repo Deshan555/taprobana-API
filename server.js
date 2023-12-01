8ff5d4384e9b69dd67a84523631e9f2b7732f86c
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-12-01 22:44:24 +0530

    update main application runner script file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { jsonParser } = require('./src/middleware/middleware');
+const endPoints = require('./src/routes/Routes');
+
+const server = express();
+const port = 3000;
+
+server.use(jsonParser);
+
+server.use('/thaprobane/core/v01', endPoints);
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
