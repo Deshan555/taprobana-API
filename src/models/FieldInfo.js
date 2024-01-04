db716864a6db89868fd01cb14df093c723623bdf
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-01-04 20:11:13 +0530

    update field info model and remove unwanted field values from model

--- src/models/FieldInfo.js
+++ src/models/FieldInfo.js
@@ -1,4 +1,5 @@
 const { query } = require('../config/database');
+const { logger } = require('../config/logger');
 
 // FieldInfoModel is an object that contains functions
 const FieldInfoModel = {
@@ -6,35 +7,58 @@ const FieldInfoModel = {
         try {
             return await query('SELECT * FROM FieldInfo');
         } catch (error) {
-            throw error;
+            logger.error('Error getting fieldInfos:', error);
         }
     },
-    addFieldInfo: async (FieldID, FieldName, FieldSize, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, RouteID, OwnerID, ZoneID) => {
+    addFieldInfo: async (FieldID, FieldName, FieldSize, FieldType, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, FieldRegistrationDate, RouteID, OwnerID, ZoneID, FactoryID) => {
         try {
-            return await query('INSERT INTO FieldInfo (FieldID, FieldName, FieldSize, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, RouteID, OwnerID, ZoneID) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)', [FieldID, FieldName, FieldSize, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, RouteID, OwnerID, ZoneID]);
+           return await query('INSERT INTO FieldInfo (FieldID, FieldName, FieldSize, FieldType, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, FieldRegistrationDate, RouteID, OwnerID, ZoneID, FactoryID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+               [FieldID, FieldName, FieldSize, FieldType, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, FieldRegistrationDate, RouteID, OwnerID, ZoneID, FactoryID]);
         } catch (error) {
-            throw error;
+            logger.error('Error adding fieldInfo:', error);
         }
     },
     getFieldInfoByID: async (FieldID) => {
         try {
             return await query('SELECT * FROM FieldInfo WHERE FieldID = ?', [FieldID]);
         } catch (error) {
-            throw error;
+            logger.error('Error getting fieldInfo by ID:', error);
         }
     },
-    updateFieldInfo: async (FieldID, FieldName, FieldSize, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, RouteID, OwnerID, ZoneID) => {
+    getFieldsByZoneID: async (ZoneID) => {
         try {
-            return await query('UPDATE FieldInfo SET FieldName = ?, FieldSize = ?, FieldAddress = ?, TeaType = ?, BaseLocation = ?, BaseElevation = ?, SoilType = ?, Attitude = ?, Longitude = ?, RouteID = ?, OwnerID = ?, ZoneID = ? WHERE FieldID = ?', [FieldName, FieldSize, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, RouteID, OwnerID, ZoneID, FieldID]);
+            return await query('SELECT * FROM FieldInfo WHERE ZoneID = ?', [ZoneID]);
         } catch (error) {
-            throw error;
+            logger.error('Error getting fieldInfo by ZoneID:', error);
+        }
+    },
+    getFieldsByFactoryID: async (FactoryID) => {
+        try {
+            return await query('SELECT * FROM FieldInfo WHERE FactoryID = ?', [FactoryID]);
+        } catch (error) {
+            logger.error('Error getting fieldInfo by FactoryID:', error);
+        }
+    },
+    getFieldsByRouteID: async (RouteID) => {
+        try {
+            return await query('SELECT * FROM FieldInfo WHERE RouteID = ?', [RouteID]);
+        } catch (error) {
+            logger.error('Error getting fieldInfo by RouteID:', error);
+        }
+    },
+    updateFieldInfo: async (FieldID,  FieldSize, FieldType, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, RouteID, OwnerID, ZoneID, FactoryID) => {
+        try {
+            return await query('UPDATE FieldInfo SET FieldSize = ?, FieldType = ?, FieldAddress = ?, TeaType = ?, BaseLocation = ?, BaseElevation = ?, SoilType = ?, Attitude = ?, Longitude = ?, RouteID = ?, OwnerID = ?, ZoneID = ?, FactoryID = ? WHERE FieldID = ?',
+                [FieldSize, FieldType, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, RouteID, OwnerID, ZoneID, FactoryID, FieldID]);
+        } catch (error) {
+            logger.error('Error updating fieldInfo:', error);
         }
     },
     deleteFieldInfo: async (FieldID) => {
         try {
             return await query('DELETE FROM FieldInfo WHERE FieldID = ?', [FieldID]);
         } catch (error) {
-            throw error;
+            logger.error('Error deleting fieldInfo:', error);
         }
     }
 };
