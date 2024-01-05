c4219e6c6d8e012ffd92b7a9358a6ac64d94480f
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-01-05 19:22:27 +0530

    improve employee model with logger addon

--- src/models/Employees.js
+++ src/models/Employees.js
@@ -1,56 +1,50 @@
 const { query } = require('../config/database');
+const logger = require("../config/logger");
 
 // EmployeeModel is an object that contains functions
 const EmployeeModel = {
     getAllEmployees: async () => {
         try {
-            const results = await query('SELECT * FROM Employees');
-            return results;
+            return await query('SELECT * FROM Employees');
         } catch (error) {
-            throw error;
+            logger.error('Error getting Employees:', error);
         }
     },
-    // EmployeeID, EmployeeName, EmployeeMobile, EmployeeAddress, EmployeeEmail, EmployeeType, RegistrationDate, FactoryID, hashedPassword
     addEmployee: async (EmployeeID, EmployeeName, JoiningDate, Email, Mobile, Role, FactoryID, Password) => {
         try {
-            const results = await query('INSERT INTO Employees (' +
+            return await query('INSERT INTO Employees (' +
                 'EmployeeID, EmployeeName, JoiningDate, Email, Mobile, RoleID, FactoryID, Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
                 [EmployeeID, EmployeeName, JoiningDate, Email, Mobile, Role, FactoryID, Password]);
-            return results;
         } catch (error) {
-            throw error;
+            logger.error('Error adding Employee:', error);
         }
     },
     getEmployeeByEmail: async (Email) => {
         try {
-            const results = await query('SELECT * FROM Employees WHERE Email = ?', [Email]);
-            return results;
+            return await query('SELECT * FROM Employees WHERE Email = ?', [Email]);
         } catch (error) {
-            throw error;
+            logger.error('Error getting Employee by Email:', error);
         }
     },
     getEmployeeByID: async (EmployeeID) => {
         try {
-            const results = await query('SELECT * FROM Employees WHERE EmployeeID = ?', [EmployeeID]);
-            return results;
+            return await query('SELECT * FROM Employees WHERE EmployeeID = ?', [EmployeeID]);
         } catch (error) {
-            throw error;
+            logger.error('Error getting Employee by ID:', error);
         }
     },
     updateEmployee: async (EmployeeID, EmployeeName, JoiningDate, Email, Mobile, Role, FactoryID) => {
         try {
-            const results = await query('UPDATE Employees SET EmployeeName = ?, JoiningDate = ?, Email = ?, Mobile = ?, Role = ?, FactoryID = ? WHERE EmployeeID = ?', [EmployeeName, JoiningDate, Email, Mobile, Role, FactoryID, EmployeeID]);
-            return results;
+            return await query('UPDATE Employees SET EmployeeName = ?, JoiningDate = ?, Email = ?, Mobile = ?, Role = ?, FactoryID = ? WHERE EmployeeID = ?', [EmployeeName, JoiningDate, Email, Mobile, Role, FactoryID, EmployeeID]);
         } catch (error) {
-            throw error;
+            logger.error('Error updating Employee:', error);
         }
     },
     deleteEmployee: async (EmployeeID) => {
         try {
-            const results = await query('DELETE FROM Employees WHERE EmployeeID = ?', [EmployeeID]);
-            return results;
+            return await query('DELETE FROM Employees WHERE EmployeeID = ?', [EmployeeID]);
         } catch (error) {
-            throw error;
+            logger.error('Error deleting Employee:', error);
         }
     }
 };
