10905b93814883357a0334f35282979e77fda1a5
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-01-05 18:54:42 +0530

    update vehicle mapping service model structure

--- src/services/VehicleMappingService.js
+++ src/services/VehicleMappingService.js
@@ -1,5 +1,10 @@
 const VehicleMappingsModel = require('../models/VehicalMappings');
+const EmployeeModel = require('../models/Employees');
+const FactoryModel = require('../models/Factory');
+const RouteModel = require('../models/RoadRouting');
 const { successResponse, errorResponse } = require('../utils/responseUtils');
+const logger = require('../config/logger');
+const RoleModel = require('../models/Roles');
 
 const VehicleMappingsController = {
     getAllVehicleMappings: async (req, res) => {
@@ -8,57 +13,108 @@ const VehicleMappingsController = {
             if(results.length === 0) return errorResponse(res, 'No vehicleMappings found', 404);
             successResponse(res, 'VehicleMappings retrieved successfully', results)
         } catch (error) {
-            console.error('Error getting vehicleMappings:', error);
+            logger.error('Error getting vehicleMappings:', error);
             errorResponse(res, 'Error Occurred while fetching vehicleMappings : '+error);
         }
     },
     addVehicleMappings: async (req, res) => {
-        const { VehicleMappingID, VehicleID, DriverID, FactoryID } = req.body;
-
-        if (!VehicleMappingID || !VehicleID || !DriverID || !FactoryID) {
-            return errorResponse(res, 'VehicleMappingID, VehicleID, DriverID and FactoryID are required fields', 400);
+        const {
+            VehicleNumber,
+            VehicleType,
+            VolumeCapacity,
+            WeightCapacity,
+            NumberPlateID,
+            FactoryID,
+            DriverID,
+            RouteID
+        } = req.body;
+        const VehicleID = Math.floor(Math.random() * 1000000000);
+        if (!VehicleNumber || !VehicleType || !VolumeCapacity || !WeightCapacity || !NumberPlateID || !FactoryID || !DriverID || !RouteID) {
+            return errorResponse(res, 'VehicleNumber, VehicleType, VolumeCapacity, WeightCapacity, NumberPlateID, FactoryID, DriverID and RouteID are required fields', 400);
         }
         try {
-            const result = await VehicleMappingsModel.addVehicleMappings(VehicleMappingID, VehicleID, DriverID, FactoryID);
-            successResponse(res, 'VehicleMappings added successfully', result);
+            const factory = await FactoryModel.getFactoryByID(FactoryID);
+            if (factory.length === 0) return errorResponse(res, 'Factory not found', 404);
+            const driver = await EmployeeModel.getEmployeeByID(DriverID);
+            if (driver.length === 0) return errorResponse(res, 'Given Employee ID not found', 404);
+            const roleValidation = await RoleModel.getRoleByID(driver[0].RoleID);
+            if (roleValidation[0].RoleName !== 'ROLE.DRIVER') return errorResponse(res, 'That employee is not a driver', 400);
+            const roadRouting = await RouteModel.getRoadRoutingByID(RouteID);
+            if (roadRouting.length === 0) return errorResponse(res, 'RoadRouting not found', 404);
+            const result = await VehicleMappingsModel.addVehicleMappings(VehicleID, VehicleNumber, VehicleType, VolumeCapacity, WeightCapacity, NumberPlateID, FactoryID, DriverID, RouteID);
+            const vehicleMappings = await VehicleMappingsModel.getVehicleMappingsByID(VehicleID);
+            if (result.affectedRows === 0) return errorResponse(res, 'Error adding vehicleMappings', 500);
+            const response = {
+                vehicleMappings: vehicleMappings,
+                factory: factory,
+                driver: driver,
+                roadRouting: roadRouting
+            };
+            successResponse(res, 'Register New VehicleMappings successfully', response);
         } catch (error) {
-            console.error('Error adding vehicleMappings:', error);
-            errorResponse(res, 'Error Occurred while adding vehicleMappings : '+error);
+            logger.error('Error adding vehicleMappings:', error);
+            errorResponse(res, 'Error Occurred while adding vehicleMappings : ' + error);
         }
-    },
-    getVehicleMappingsByID: async (req, res) => {
-        const {VehicleMappingID} = req.params;
-        try {
-            const results = await VehicleMappingsModel.getVehicleMappingsByID(VehicleMappingID);
-            if (results.length === 0) return errorResponse(res, 'VehicleMappings not found', 404);
-            successResponse(res, 'VehicleMappings retrieved successfully', results);
-        } catch (error) {
-            console.error('Error getting vehicleMappings by ID:', error);
-            errorResponse(res, 'Error Occurred while fetching vehicleMappings by ID : '+error);
-        }
-
     },
     updateVehicleMappings: async (req, res) => {
-        const {VehicleMappingID} = req.params;
-        const {VehicleID, DriverID, FactoryID} = req.body;
+        const {VehicleID} = req.params;
+        const {
+            VehicleNumber,
+            VehicleType,
+            VolumeCapacity,
+            WeightCapacity,
+            NumberPlateID,
+            FactoryID,
+            DriverID,
+            RouteID
+        } = req.body;
         try {
-            const result = await VehicleMappingsModel.updateVehicleMappings(VehicleMappingID, VehicleID, DriverID, FactoryID);
-            successResponse(res, 'VehicleMappings updated successfully', result);
+            const vehicleMappings = await VehicleMappingsModel.getVehicleMappingsByID(VehicleID);
+            if(vehicleMappings.length === 0) return errorResponse(res, 'VehicleMappings not found', 404);
+            const factory = await FactoryModel.getFactoryByID(FactoryID);
+            if (factory.length === 0) return errorResponse(res, 'Factory not found', 404);
+            const driver = await EmployeeModel.getEmployeeByID(DriverID);
+            if (driver.length === 0) return errorResponse(res, 'Driver not found', 404);
+            const roleValidation = await RoleModel.getRoleByID(driver[0].RoleID);
+            if (roleValidation[0].RoleName !== 'ROLE.DRIVER') return errorResponse(res, 'That employee is not a driver', 400);
+            const roadRouting = await RouteModel.getRoadRoutingByID(RouteID);
+            if (roadRouting.length === 0) return errorResponse(res, 'RoadRouting not found', 404);
+            const result = await VehicleMappingsModel.updateVehicleMappings(VehicleID, VehicleNumber, VehicleType, VolumeCapacity, WeightCapacity, NumberPlateID, FactoryID, DriverID, RouteID);
+            const getVehicleMappingsByID = await VehicleMappingsModel.getVehicleMappingsByID(VehicleID);
+            if (result.affectedRows === 0) return errorResponse(res, 'Error updating vehicleMappings', 500);
+            const response = {
+                vehicleMappings: getVehicleMappingsByID,
+                factory: factory,
+                driver: driver,
+                roadRouting: roadRouting
+            };
+            successResponse(res, 'VehicleMappings updated successfully', response);
         } catch (error) {
-            console.error('Error updating vehicleMappings:', error);
-            errorResponse(res, 'Error Occurred while updating vehicleMappings : '+error);
+            logger.error('Error updating vehicleMappings:', error);
+            errorResponse(res, 'Error Occurred while updating vehicleMappings : ' + error);
         }
     },
     deleteVehicleMappings: async (req, res) => {
-        const {VehicleMappingID} = req.params;
+        const {VehicleID} = req.params;
         try {
-            await VehicleMappingsModel.deleteVehicleMappings(VehicleMappingID);
+            await VehicleMappingsModel.deleteVehicleMappings(VehicleID);
             successResponse(res, 'VehicleMappings deleted successfully', null);
         } catch (error) {
-            console.error('Error deleting vehicleMappings:', error);
+            logger.error('Error deleting vehicleMappings:', error)
             errorResponse(res, 'Error Occurred while deleting vehicleMappings : ' + error);
         }
+    },
+    getAllVehicleMappingsByID: async (req, res) => {
+        const {VehicleID} = req.params;
+        try {
+            const results = await VehicleMappingsModel.getVehicleMappingsByID(VehicleID);
+            if(results.length === 0) return errorResponse(res, 'VehicleMappings not found', 404);
+            successResponse(res, 'VehicleMappings retrieved successfully', results);
+        } catch (error) {
+            logger.error('Error getting vehicleMappings by ID:', error);
+            errorResponse(res, 'Error Occurred while fetching vehicleMappings by ID : ' + error);
+        }
     }
-};
+}
 
 module.exports = VehicleMappingsController;
