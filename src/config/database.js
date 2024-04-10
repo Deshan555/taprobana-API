a09678ce9df6021b5fdaea502672a3d172342b77
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-10 09:14:05 +0530

    Update database configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,36 @@
 // Desc: Database configuration
+// const mysql = require('mysql');
+// const util = require('util');
+//
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'TeaCooperative',
+// });
+//
+// // Promisify the query method
+// const query = util.promisify(db.query).bind(db);
+//
+// db.connect(err => {
+//     if (err) {
+//         console.error('Database connection failed:', err);
+//     } else {
+//         console.log('Connected to the database');
+//     }
+// });
+//
+// module.exports = { db, query };
+
 const mysql = require('mysql');
 const util = require('util');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'TeaCooperative',
+    host: 'mysql-146534cc-maxwon555-4b2b.a.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_HkFtwGVfYhp952RnHRd',
+    database: 'teacooperative',
+    port: '15864',
 });
 
 // Promisify the query method
@@ -21,3 +45,5 @@ db.connect(err => {
 });
 
 module.exports = { db, query };
+
+
