efbb3ae6e61370c588cc6882887008376cdca444
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-10 09:17:41 +0530

    Commented out unnecessary code in FieldInfoService.js

--- src/services/FieldInfoService.js
+++ src/services/FieldInfoService.js
@@ -39,23 +39,23 @@ const FieldInfoController = {
         if (!fieldSize || !fieldType || !fieldAddress || !teaType || !baseLocation || !baseElevation || !soilType || !latitude || !longitude || !ownerID || !zoneID || !factoryID) {
             return errorResponse(res, 'FieldSize, FieldType, FieldAddress, TeaType, BaseLocation, BaseElevation, SoilType, Attitude, Longitude, OwnerID, ZoneID and FactoryID are required fields', 400);
         } try {
-            const roadRouting = await RouteModel.getRoadRoutingByID(routeID);
-            if(roadRouting.length === 0) return errorResponse(res, 'RoadRouting not found', 404);
-            const customer = await CustomerModel.getCustomerByID(ownerID);
-            if(customer.length === 0) return errorResponse(res, 'Customer not found', 404);
-            const environmentalZone = await ZoneModel.getEnvironmentalZoneByID(zoneID);
-            if(environmentalZone.length === 0) return errorResponse(res, 'EnvironmentalZone not found', 404);
-            const factory = await FactoryModel.getFactoryByID(factoryID);
-            if(factory.length === 0) return errorResponse(res, 'Factory not found', 404);
+            // const roadRouting = await RouteModel.getRoadRoutingByID(routeID);
+            // if(roadRouting.length === 0) return errorResponse(res, 'RoadRouting not found', 404);
+            // const customer = await CustomerModel.getCustomerByID(ownerID);
+            // if(customer.length === 0) return errorResponse(res, 'Customer not found', 404);
+            // const environmentalZone = await ZoneModel.getEnvironmentalZoneByID(zoneID);
+            // if(environmentalZone.length === 0) return errorResponse(res, 'EnvironmentalZone not found', 404);
+            // const factory = await FactoryModel.getFactoryByID(factoryID);
+            //if(factory.length === 0) return errorResponse(res, 'Factory not found', 404);
             const result = await FieldInfoModel.addFieldInfo(FieldID, FieldName, fieldSize, fieldType, fieldAddress, teaType, baseLocation, baseElevation, soilType, latitude, longitude, FieldRegistrationDate, routeID, ownerID, zoneID, factoryID);
             const fieldInfo = await FieldInfoModel.getFieldInfoByID(FieldID);
             if (result.affectedRows === 0) return errorResponse(res, 'Error adding fieldInfo', 500);
             const response = {
                 fieldInfo: fieldInfo,
-                roadRouting: roadRouting,
-                customer: customer,
-                environmentalZone: environmentalZone,
-                factory: factory
+                //roadRouting: roadRouting,
+                //customer: customer,
+                //environmentalZone: environmentalZone,
+                //factory: factory
             };
             successResponse(res, 'Register New FieldInfo successfully', response);
             logger.info('Register New FieldInfo successfully');
@@ -79,6 +79,7 @@ const FieldInfoController = {
         const {FieldID} = req.params;
         const fieldID = FieldID;
         console.log('fieldID', fieldID)
+        
         const {
             fieldSize,
             fieldType,
