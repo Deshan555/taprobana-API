83b7e374025762919c08e0d93441733879545344
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-10 09:14:20 +0530

    Update server.js file to use CORS and set port to 3001

--- src/Server.js
+++ src/Server.js
@@ -5,8 +5,11 @@ const logger = require('morgan');
 const cors = require('cors');
 
 const server = express();
+const port = 3001;
+
 server.use(logger('dev'));
-const port = 3000;
+
+server.use(cors());
 
 server.use(bodyParser.json());
 
@@ -15,3 +18,12 @@ server.use('/thaprobane/core/v01', endPoints);
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+// cors modifications in the server.js file
+// server.use(cors({
+//     origin: 'http://your_frontend_domain.com',  // Replace with the domain of your frontend
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,  // Enable credentials (cookies, authorization headers, etc.)
+//     optionsSuccessStatus: 204,  // Some legacy browsers (IE11, various SmartTVs) choke on 204
+// }));
