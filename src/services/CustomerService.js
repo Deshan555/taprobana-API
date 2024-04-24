f8bcf63dfd76c0644be0156c4cfbe7eefac051e0
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-04-24 20:21:04 +0530

    Refactor logging statements in DailyTeaCollectionController to use logger instead of console.error

--- src/services/CustomerService.js
+++ src/services/CustomerService.js
@@ -1,8 +1,10 @@
 const CustomerModel = require('../models/Customers');
 const { successResponse, errorResponse } = require('../utils/responseUtils');
 const {hashPassword} = require('../utils/bcrypt');
+const bcrypt = require('../utils/bcrypt');
 const EmailService = require('../services/MailService');
 const TemplateProvider = require('../services/TemplateProvider');
+const { logger } = require('../config/transporter');
 
 const CustomerController = {
     sampleEndPoint: async (req, res) => {
@@ -32,9 +34,8 @@ const CustomerController = {
         }
     },
     addCustomer: async (req, res) => {
-        // console.log(req);
         const { customerName, customerMobile, customerAddress, customerEmail, customerType, customerPassword, factoryID, customerNIC } = req.body;
-        console.log( customerName, customerMobile, customerAddress, customerEmail, customerType, customerPassword, factoryID)
+        console.log(customerName, customerMobile, customerAddress, customerEmail, customerType, customerPassword, factoryID)
         if (!customerName || !customerMobile || !customerAddress || !customerEmail || !customerType || !customerPassword || !factoryID || !customerNIC) {
             return errorResponse(res, 'CustomerName, CustomerMobile, CustomerAddress, CustomerEmail, CustomerType, PasswordOfCustomer, NIC and FactoryID are required fields', 400);
         }
@@ -52,11 +53,18 @@ const CustomerController = {
             const RegistrationDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
             const hashedPassword = await hashPassword(customerPassword);
             const result = await CustomerModel.addCustomer(CustomerID, customerName, customerMobile, customerAddress, customerEmail, customerType, RegistrationDate, hashedPassword, customerNIC, factoryID);
-
             if (result.affectedRows === 0)
                 return errorResponse(res, 'Error adding customer', 500);
             else if (result.affectedRows === 1)
                 affectedCustomer = await CustomerModel.getCustomerByID(CustomerID);
+
+            const templateProvider = TemplateProvider.genarateRegisterCustomer(customerName, customerEmail, customerPassword);
+            EmailService.sendSingleEmail({
+                to: customerEmail,
+                subject: 'Customer Registration',
+                text: '',
+                html: templateProvider
+            });
             return successResponse(res, 'Customer added successfully', affectedCustomer[0]);
 
         } catch (error) {
@@ -82,6 +90,7 @@ const CustomerController = {
                     const hashedPassword = await hashPassword(userPassword);
                     try{
                         const result = await CustomerModel.addCustomer(CustomerID, data[i].customerName, data[i].customerMobile, data[i].customerAddress, data[i].customerEmail, data[i].customerType, RegistrationDate, hashedPassword, data[i].customerNIC, data[i].factoryID);
+                        logger.info(result);
                         const templateProvider = TemplateProvider.genarateRegisterCustomer(data[i].customerName, data[i].customerEmail, userPassword);
                         EmailService.sendSingleEmail({
                             to: data[i].customerEmail,
@@ -126,12 +135,18 @@ const CustomerController = {
             CustomerAddress,
             CustomerEmail,
             CustomerType,
-            RegistrationDate,
             FactoryID
         } = req.body;
         console.log(CustomerName, CustomerMobile, CustomerAddress, CustomerEmail, CustomerType, FactoryID);
         try {
             const result = await CustomerModel.updateCustomer(CustomerID, CustomerName, CustomerMobile, CustomerAddress, CustomerEmail, CustomerType ,FactoryID);
+            const templateProvider = TemplateProvider.genrateProfileUpdateTemplate(CustomerName, CustomerEmail);
+            EmailService.sendSingleEmail({
+                to: CustomerEmail,
+                subject: 'Customer Profile Update',
+                text: '',
+                html: templateProvider
+            });
             successResponse(res, 'Customer updated successfully', result);
         } catch (error) {
             console.error('Error updating customer:', error);
@@ -147,7 +162,74 @@ const CustomerController = {
             console.error('Error deleting customer:', error);
             errorResponse(res, 'Error Occurred while deleting customer : ' + error);
         }
+    },
+    // employeePasswordUpdate : async (req, res) => {
+    //     const {EmployeeID, newPassword, oldPassword, email} = req.body;
+
+    //     console.log(EmployeeID, newPassword, oldPassword, email);
+    //     try{
+    //         const results = await EmployeeModel.getEmployeeByEmail(email);
+    //         let passwordFromDataBase = '';
+    //         if(results.length === 0)
+    //             return errorResponse(res, 'Can Not Find Employee With Given Email Address', 404);
+    //         else
+    //             passwordFromDataBase = results[0].Password;
+    //             const passwordMatch = await bcrypt.comparePassword(oldPassword, passwordFromDataBase);
+    //             if(passwordMatch === false)
+    //                 return errorResponse(res, 'Invalid Credentials, Old Wrong Password Provided', 401);
+
+    //             const hashedPassword = await bcrypt.hashPassword(newPassword);
+    //             const updateResult = await EmployeeModel.updateEmployeePassword(EmployeeID, hashedPassword);
+    //             if(updateResult?.affectedRows === 0)
+    //                 return errorResponse(res, 'Error Updating Password', 500);
+    //             successResponse(res, 'Password Updated Successfully', null);
+    //     }
+    //     catch (error) {
+    //         console.error('Error getting employee by email:', error);
+    //         errorResponse(res, 'Error Occurred while fetching employee by email : '+error);
+    //     }
+    // }
+    updatePasswordCustomer: async (req, res) => {
+        const {customerID, newPassword, oldPassword, email} = req.body;
+        try {
+            const results = await CustomerModel.getCustomerByEmail(email);
+            let passwordFromDataBase = '';
+            if(results.length === 0)
+                return errorResponse(res, 'Can Not Find Customer With Given Email Address', 404);
+            else
+                passwordFromDataBase = results[0].Password;
+                const passwordMatch = await bcrypt.comparePassword(oldPassword, passwordFromDataBase);
+                if(passwordMatch === false)
+                    return errorResponse(res, 'Invalid Credentials, Wrong Password', 401);
+
+                const hashedPassword = await bcrypt.hashPassword(newPassword);
+                const updateResult = await CustomerModel.updateCustomerPassword(customerID, hashedPassword);
+                if(updateResult?.affectedRows === 0)
+                    return errorResponse(res, 'Error Updating Password', 500);
+                successResponse(res, 'Password Updated Successfully', null);
+        } catch (error) {
+            errorResponse(res, 'Error Occurred while fetching customer by email : '+error);
+        }
+    },
+forcePasswordChange: async (req, res) => {
+    const { email } = req.body;
+    try {
+        const results = await CustomerModel.getCustomerByEmail(email);
+        const customerID = results[0].CustomerID;
+        if(results.length === 0)
+            return errorResponse(res, 'Can Not Find Customer With Given Email Address', 404);
+
+        const newPassword = Math.random().toString(36).slice(-8);
+        console.log(newPassword);
+        const hashedPassword = await bcrypt.hashPassword(newPassword);
+        const updateResult = await CustomerModel.updateCustomerPassword(customerID, hashedPassword);
+        if(updateResult?.affectedRows === 0)
+            return errorResponse(res, 'Error Updating Password', 500);
+        successResponse(res, 'Password Updated Successfully', newPassword);
+    } catch (error) {
+        errorResponse(res, 'Error Occurred while updating password : '+error);
     }
+}
 };
 
 module.exports = CustomerController;
